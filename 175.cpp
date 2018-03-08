00f4b1b23b12ac2c48d4cfc5d51d4c2916ac3184
ASHISH JHA <amiableashish55@gmail.com>
2018-03-08 23:25:34 +0530

    2nd

---
+++ 175.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m>>n;
+        m=m^n;
+        cout<<(log2(m & -m)+1)<<endl;
+
+    }
+}

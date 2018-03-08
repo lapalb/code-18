9599b62e9574ca222a0b2394821523bf872204fc
ASHISH JHA <amiableashish55@gmail.com>
2018-03-08 23:16:31 +0530

    176th GFG

---
+++ 176.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int cnt=0;
+        while(n)
+        {
+            cnt++;
+            if(n&1==1)
+            {
+                cout<<cnt<<endl;
+                break;
+            }
+            n=n>>1;
+        }
+        if(n==0)cout<<0<<endl;
+    }
+}

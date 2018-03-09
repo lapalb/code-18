bec484124013f6a4bf8a68ff72164db7ec65555d
ASHISH JHA <amiableashish55@gmail.com>
2018-03-09 17:45:28 +0530

    Create 168.cpp

---
+++ 168.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int n;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    sum+=(__builtin_popcount(i));
+
+    cout<<sum<<endl;
+}
+}

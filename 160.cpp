4e418f0baa1c6300571715d7168a586af27cd931
ASHISH JHA <amiableashish55@gmail.com>
2018-03-25 19:39:47 +0530

    https://practice.geeksforgeeks.org/problems/tom-and-jerry/0

---
+++ 160.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        a[1]=
+        rep(i,1,n+1)
+        {
+            if(n%i==0)
+        }
+    }
+}

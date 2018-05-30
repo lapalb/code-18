7d341148e5e04cea27e0f2e7279354a7d2f8c2ff
ASHISH JHA <115CS0603@nitrkl.ac.in>
2018-05-30 10:07:10 +0530

    Revised the synatx of struct

---
+++ Struct.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+struct 
+{
+   char name[50];
+   char street[100];
+   char city[50];
+   char state[20];
+   int pin;
+} s1,s2;
+int main()
+{
+    s1.name="Darbhanga"
+    printf("Hello World");
+
+    return 0;
+}
